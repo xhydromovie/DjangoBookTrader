ce2c573575875c1c993e4caa22ec9ea558a932b0
KompKK <komkk@protonmail.com>
2018-10-20 19:00:26 +0200

    Added login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .forms import Register_form
+from .forms import Register_form, Login_form
 
 
 def register(request):
     form = Register_form()
     return render(request, 'accounts/register.html', {'form': form})
+
+def login(request):
+    return render(request, 'accounts/login.html', {'form': Login_form()})
