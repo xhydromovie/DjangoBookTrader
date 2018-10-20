a3ef666db707f78b304aa2fa0a18e7b5de894205
Kacper Szot <kacperszot1@gmail.com>
2018-10-20 15:20:33 +0200

    added Book Model

--- trader/models.py
+++ trader/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    """
+    Model of book that contains information about book like: author name, book title, ISBN, publication date
+    """
+    # Author name
+    author = models.CharField(max_length=50)
+    # Book title
+    title = models.CharField(max_length=50)
+    # ISBN
+    isbn = models.CharField(max_length=50)
+    # Publication date
+    publication_date = models.DateField('publication_date')
+
+    def __str__(self):
+        """
+        Method returns Book Title
+        :return: Book Title
+        """
+        return self.title
