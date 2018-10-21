88898e9d9dfcda72a21a6f171c4dc487b7b8e153
Kacper Szot <kacperszot1@gmail.com>
2018-10-21 12:04:06 +0200

    typo

--- trader/urls.py
+++ trader/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('advertisement-create/', views.advertisement_creat, name='advertisement-creat'),
+    path('advertisement-create/', views.advertisement_create, name='advertisement-creat'),
     path('advertisement/<int:id>/report', views.report, name='report'),
     path('advertisement/<int:adv_id>', views.advertisement, name='advertisement')
 ]
