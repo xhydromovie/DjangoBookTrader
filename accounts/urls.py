c532187e7e6c066129e16ce93ea6c6e7d8513bfb
KompKK <komkk@protonmail.com>
2018-10-21 12:38:14 +0200

    Profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.register, name="register"),
     path('login/', views.login, name="login"),
     path('profile/', views.myprofile, name="myprofile"),
+    path('profile/<int:id>/', views.profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import Register_form, Login_form
+from django.http import Http404
+from django.contrib.auth.models import User
 
 
 def register(request):
@@ -11,3 +13,10 @@ def login(request):
 
 def myprofile(request):
     return render(request, 'accounts/profile.html', {'user': request.user})
+
+def profile(request, id):
+    try:
+        user = User.objects.get(pk=id)
+    except User.DoesNotExist:
+        raise Http404('Nie ma takiego usera')
+    return render(request, 'accounts/profile.html', {'user': user})
